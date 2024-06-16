80f4f89f34b352b785be48110630d697b56bc8d4
nisioka <16322296+nisioka@users.noreply.github.com>
2024-06-16 21:55:17 +0900

    CHALLENGE back api use.

--- gas-spa/apps-script/Code.ts
+++ gas-spa/apps-script/Code.ts
@@ -1,6 +1,21 @@
 function doGet() {
-  // @ts-ignore
   return HtmlService.createHtmlOutputFromFile("index.html")
   .addMetaTag("viewport", "width=device-width, initial-scale=1")
   .setTitle("GAS-SPA-template");
+}
+
+function doPost(e: GoogleAppsScript.Events.DoPost) {
+  console.log(e.postData.name)
+  console.log(e.parameter.toString())
+  switch (e.postData.name) {
+    default:
+      return getList();
+  }
+}
+
+function getList() {
+  return [
+    {title: "title1", text: "text1"},
+    {title: "title2", text: "text2"}
+  ];
 }
--- gas-spa/src/components/Content.tsx
+++ gas-spa/src/components/Content.tsx
@@ -1,15 +1,30 @@
 import BodyCard from "./BodyCard.tsx";
 import {Grid} from "@mui/material";
+import {useEffect, useState} from "react";
 
+type Content = {
+  id: number,
+  title: string,
+  text: string
+
+}
 export default function Content() {
+  const [content, setContent] = useState([] as Content[]);
+  useEffect(() => {
+    fetch("/getList", {method: "POST", headers: {"Content-Type": "application/json"}})
+        .then(response => response.json() as Promise<Content[]>)
+        .then(data => setContent(data));
+  });
+  function getCardContent(object: Content) {
+    return (
+        <Grid item xs={12} sm={4} key={object.id}>
+          <BodyCard title={object.title} text={object.text} />
+        </Grid>
+    );
+  }
   return (
       <Grid container spacing={2}>
-        <Grid item sm={3}>
-          <BodyCard title={"title1"} text={"text1"}/>
-        </Grid>
-        <Grid item sm={3}>
-          <BodyCard title={"title2"} text={"text2"}/>
-        </Grid>
+        {content.map(c => getCardContent(c))}
       </Grid>
   );
 }
